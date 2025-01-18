5c0b571c4c77aae4f4c78a741ebd3de492bf8a4d
KauaFMarques <kauamarques52@outlook.com>
2025-01-18 11:58:29 -0300

    alterando o status de created

--- src/main/kotlin/com/mercadolivro/mercado/livro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/mercado/livro/controller/CustomerController.kt
@@ -2,10 +2,12 @@ package com.mercadolivro.mercado.livro.controller
 
 import com.mercadolivro.mercado.livro.model.CustomerModel
 import com.mercadolivro.mercado.livro.request.PostCustomerRequest
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,8 +19,8 @@ class CustomerController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer:PostCustomerRequest){
         println(customer)
-        println("hello world")
     }
 }
